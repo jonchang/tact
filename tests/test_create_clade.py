af85d0b4b8c6a82814a5f859900e23d103cd24b9
Jonathan Chang <jchang641@gmail.com>
2018-07-10 05:36:23 -0400

    Fix test

--- tests/test_create_clade.py
+++ tests/test_create_clade.py
@@ -7,17 +7,18 @@ from dendropy import TaxonNamespace
 
 from tact.cli_add_taxa import create_clade, count_locked, edge_iter
 
-
-
 @given(st.lists(
-    st.from_regex(r"\A[A-Z][a-z]+ [a-z]+\Z"),
-    st.floats(min_value=1e-3, max_value=1000, allow_infinity=False, allow_nan=False),
+    st.tuples(
+        st.from_regex(r"\A[A-Z][a-z]+ [a-z]+\Z"),
+        st.floats(min_value=1e-3, max_value=1000, allow_infinity=False, allow_nan=False)
+        ),
     min_size=1,
     unique_by=lambda x: x[0]
     ).map(dict))
 def test_create_clade(data):
     spp = data.keys()
     ages = data.values()
+    assume(len(set(ages)) == len(ages))
     tn = TaxonNamespace(spp, label="taxa")
     clade = create_clade(tn, spp, ages)
     xx = [x.label == "locked" for x in edge_iter(clade.seed_node)]
