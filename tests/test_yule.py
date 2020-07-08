5ff8f40b0b52b8699e12f2774117cff0423d540b
Jonathan Chang <jchang641@gmail.com>
2020-07-08 20:59:27 +1000

    Exclude 0 age for yule testing

--- tests/test_yule.py
+++ tests/test_yule.py
@@ -6,7 +6,7 @@ import hypothesis.strategies as st
 from tact.lib import optim_yule
 
 
-@given(st.lists(st.floats(min_value=0, allow_infinity=False, allow_nan=False), min_size=1), st.floats(min_value=1e-9, max_value=1))
+@given(st.lists(st.floats(min_value=0, allow_infinity=False, allow_nan=False, exclude_min=True), min_size=1), st.floats(min_value=1e-9, max_value=1))
 def test_yule_has_no_extinction(ages, sampling):
     (b, d) = optim_yule(ages, sampling)
     assert d == 0
