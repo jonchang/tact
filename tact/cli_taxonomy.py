144c8d5d5dd238d653136af8bd37ef8b37d69b91
Jonathan Chang <jchang641@gmail.com>
2020-07-14 17:09:45 +1000

    Overhaul tact_build_taxonomic_tree
    It will now automatically do a few things:
    
    * taxonomic name mangling for duplicated names
    * detection of empty spreadsheet cells
    * sorting of input

--- tact/cli_taxonomy.py
+++ tact/cli_taxonomy.py
@@ -1,65 +1,116 @@
 import csv
+import collections
 
 import dendropy
 import click
 
-
-def wc(filename):
+def fix_file(filename):
     """
-    Get number of lines in a file. This should be relatively cheap,
-    and it might put the file in the I/O cache.
+    Slurps a file, and does various checks and fixes:
+    * Sorts the file
+    * Ensures column names are unique
     """
     with open(filename, "rU") as rfile:
-        for ii, _ in enumerate(rfile):
-            pass
-        return ii + 1
+        lines = rfile.readlines()
 
+    heads = collections.defaultdict(int)
+    for item in lines[0].split(","):
+        heads[item] += 1
+    bad_heads = []
+    for key, value in heads.items():
+        if value > 1:
+            bad_heads.append(key)
+
+    if len(bad_heads) > 0:
+        raise click.UsageError("CSV headers must have unique names. Duplicated column names:\n*  {}".format("\n*  ".join(bad_heads)))
+
+    return [lines[0], *sorted(lines[1:])]
+
+def ensure(st, ctx=""):
+    "Ensures that a cell is not empty."
+    if len(st) == 0:
+        if len(ctx) > 0:
+            text = " Offending line:\n{}".format(",".join(line))
+        raise click.UsageError("All cells in the CSV must be nonempty." + text)
+
+def mangle_rank(row, names):
+    seen = set()
+    new = []
+    for idx, item in enumerate(row):
+        if item in seen:
+            if idx >= len(names):
+                raise click.UsageError("Invalid name (possibly duplicated): {}\nSeen at: {}".format(names, ",".join(row)))
+            item = item + "__" + names[idx] + "__"
+        seen.add(item)
+        new.append(item)
+    return new
 
 def build_taxonomic_tree(filename):
     """
     Builds a taxonomic tree given a filename. Last column is assumed to
-    be a species name and the file must be sorted. All ranks must nest
-    completely within the next highest rank.
+    be a species name. All ranks must nest completely within the next
+    highest rank.
     """
-    lines = wc(filename)
-    with open(filename, "rU") as rfile:
-        reader = csv.reader(rfile)
+    lines = fix_file(filename)
+    reader = csv.reader(lines)
 
-        rank_names = next(reader)
-        rank_names.pop()  # assume last column is species name
+    rank_names = next(reader)
+    rank_names.pop()  # assume last column is species name
 
-        tree = dendropy.Tree()
-        node = tree.seed_node
-        tn = tree.taxon_namespace
-        tn.is_mutable = True
+    tree = dendropy.Tree()
+    node = tree.seed_node
+    tn = tree.taxon_namespace
+    tn.is_mutable = True
+    node.taxon = tn.new_taxon("__TAXONOMIC_ROOT__")
 
-        row = next(reader)
-        for col in row:
-            if col:
-                node = node.new_child(taxon=tn.new_taxon(col))
+    # Mangle and uniquify the first row
+    mangled_ranks = set()
+    row = next(reader)
+    mangled_row = mangle_rank(row, rank_names)
+    for orig, new in zip(row, mangled_row):
+        if orig != new:
+            mangled_ranks.add((orig, new))
+    row = mangled_row
 
-        to_add = list()
-        stack = row
-        known_nodes = dict()
-        with click.progressbar(enumerate(reader), label="Generating taxonomy", length=lines) as rf:
-            for idx, row in rf:
-                for prev, cur in zip(reversed(stack), reversed(row)):
-                    if prev == cur and prev != "" and cur != "":
-                        break
-                    else:
-                        to_add.append(cur)
-                if prev in known_nodes:
-                    node = known_nodes[prev]
+    # Initialize the tree structure
+    for col in row:
+        ensure(col, ctx=row)
+        node = node.new_child(taxon=tn.new_taxon(col))
+
+    to_add = list()
+    stack = row
+    known_nodes = dict()
+    with click.progressbar(enumerate(reader), label="Generating taxonomy", length=len(lines)) as rf:
+        for idx, row in rf:
+            # Uniquify row names
+            mangled_row = mangle_rank(row, rank_names)
+            for orig, new in zip(row, mangled_row):
+                if orig != new:
+                    mangled_ranks.add((orig, new))
+            row = mangled_row
+
+            for prev, cur in zip(reversed(stack), reversed(row)):
+                ensure(cur, ctx=row)
+                if prev == cur:
+                    break
                 else:
-                    node = tree.find_node_with_taxon_label(prev)
-                    known_nodes[prev] = node
-                while to_add:
-                    nt = to_add.pop()
-                    if nt:
-                        node = node.new_child(taxon=tn.new_taxon(nt))
-                stack = row
-        tn.is_mutable = False
-        return tree
+                    to_add.append(cur)
+            if prev in known_nodes:
+                node = known_nodes[prev]
+            else:
+                node = tree.find_node_with_taxon_label(prev)
+                known_nodes[prev] = node
+            while to_add:
+                nt = to_add.pop()
+                if nt:
+                    node = node.new_child(taxon=tn.new_taxon(nt))
+            stack = row
+    tn.is_mutable = False
+    if len(mangled_ranks) > 0:
+        click.echo("Note: several rank names were adjusted to ensure uniqueness. These are:")
+        for orig, new in mangled_ranks:
+            click.echo("{} => {}".format(orig, new))
+    return tree
 
 
 @click.command()
@@ -69,11 +120,18 @@ def build_taxonomic_tree(filename):
 def main(taxonomy, output, schema):
     """Generates a taxonomic tree from TAXONOMY.
 
-    TAXONOMY is formatted as a CSV file where each column is a taxonomic
-    rank (from most inclusive to least inclusive, with the last column as the
-    species name) and each row is a separate species. The CSV must be sorted.
-    Each rank must be named (i.e., there should be no empty cells in the
-    spreadsheet) and unique.
+    TAXONOMY is a comma-separated values file with several requirements.
+
+    * Each row represents a single species.
+    * Each column represents a taxonomic rank.
+    * The columns must be arranged from most inclusive to least inclusive,
+      with the last column as the species name.
+      - e.g. Family,Genus,Species
+    * Each rank must be named
+    * Each rank must be unique
+      - OK: Cichlidae,Cichla,Cichla temensis
+      - NO: Cichlidae,Cichlidae,Cichla temensis
+      - NO: Cichlidae,,Cichla temensis
 
     This script makes **many** assumptions about its input for speed. Check
     the example taxonomy in the examples/ folder for guidance.
