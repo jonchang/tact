2d6d233f80f5804bca0949a667c0b2da74b734d5
Jonathan Chang <me@jonathanchang.org>
2021-07-31 14:07:16 -0700

    Simplify test code

--- tests/test_bd_optim.py
+++ tests/test_bd_optim.py
@@ -1,15 +1,13 @@
 from __future__ import division
 
-from hypothesis import given, settings
+from hypothesis import given, settings, example
 import hypothesis.strategies as st
 
 from tact.lib import optim_bd
 
-@settings(max_examples=1000, deadline=5000)
-@given(st.one_of(
-    st.just([38.428566000000004, 31.664333000000003, 29.987381, 27.317966000000002, 19.033624, 10.301403, 10.292682, 7.923767, 1.740758, 1.370161, 0.831707, 0.799889, 0.518386]),
-    st.lists(st.floats(min_value=0, allow_infinity=False, allow_nan=False, exclude_min=True), min_size=1)
-    ), st.floats(min_value=1e-9, max_value=1))
+@settings(deadline=1500)
+@example([38.428566000000004, 31.664333000000003, 29.987381, 27.317966000000002, 19.033624, 10.301403, 10.292682, 7.923767, 1.740758, 1.370161, 0.831707, 0.799889, 0.518386], 0.9931334704375381)
+@given(st.lists(st.floats(min_value=0, allow_infinity=False, allow_nan=False, exclude_min=True), min_size=1), st.floats(min_value=1e-9, max_value=1))
 def test_birth_death_can_run(ages, sampling):
     b, d = optim_bd(ages, sampling)
     assert b > 0
