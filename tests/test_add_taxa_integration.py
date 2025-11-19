20b0314c30844639a0245a84bc6a8dc2c215c7ba
Jonathan Chang <me@jonathanchang.org>
2025-11-18 17:03:35 -0800

    Don't write to working dir during testing

--- tests/test_add_taxa_integration.py
+++ tests/test_add_taxa_integration.py
@@ -7,16 +7,17 @@ from dendropy import Tree
 execution_number = range(2)
 
 
-def run_tact(script_runner, datadir, stem):
+def run_tact(script_runner, datadir, stem, tmp_path):
     backbone = os.path.join(datadir, stem + ".backbone.tre")
     taxonomy = os.path.join(datadir, stem + ".taxonomy.tre")
     taxed = Tree.get(path=taxonomy, schema="newick")
     bbone = Tree.get(path=backbone, schema="newick", rooting="default-rooted")
+    output_base = tmp_path / f"tact-pytest-{stem}"
     result = script_runner.run(
-        ["tact_add_taxa", "--taxonomy", taxonomy, "--backbone", backbone, "--output", ".tact-pytest-" + stem, "-vv"]
+        ["tact_add_taxa", "--taxonomy", taxonomy, "--backbone", backbone, "--output", str(output_base), "-vv"]
     )
     assert result.returncode == 0
-    output = ".tact-pytest-" + stem + ".newick.tre"
+    output = str(output_base) + ".newick.tre"
     tacted = Tree.get(path=output, schema="newick", rooting="default-rooted")
     ss = tacted.as_ascii_plot()
     sys.stderr.write(ss)
@@ -29,7 +30,7 @@ def run_tact(script_runner, datadir, stem):
             "--backbone",
             backbone,
             "--output",
-            ".tact-pytest-" + stem + ".check.csv",
+            str(output_base) + ".check.csv",
             "--cores=1",
         ]
     )
@@ -38,11 +39,12 @@ def run_tact(script_runner, datadir, stem):
 
 
 @pytest.mark.parametrize("execution_number", execution_number)
-def test_yule(script_runner, execution_number, datadir):
+def test_yule(script_runner, execution_number, datadir, tmp_path):
     backbone = os.path.join(datadir, "stem2.backbone.tre")
     taxonomy = os.path.join(datadir, "stem2.taxonomy.tre")
     taxed = Tree.get(path=taxonomy, schema="newick")
     bbone = Tree.get(path=backbone, schema="newick", rooting="default-rooted")
+    output_base = tmp_path / "tact-pytest-yule"
     result = script_runner.run(
         [
             "tact_add_taxa",
@@ -51,13 +53,13 @@ def test_yule(script_runner, execution_number, datadir):
             "--backbone",
             backbone,
             "--output",
-            ".tact-pytest-yule",
+            str(output_base),
             "-vv",
             "--yule",
         ]
     )
     assert result.returncode == 0
-    output = ".tact-pytest-yule.newick.tre"
+    output = str(output_base) + ".newick.tre"
     tacted = Tree.get(path=output, schema="newick", rooting="default-rooted")
     ss = tacted.as_ascii_plot()
     sys.stderr.write(ss)
@@ -70,18 +72,17 @@ def test_yule(script_runner, execution_number, datadir):
             "--backbone",
             backbone,
             "--output",
-            ".tact-pytest-yule.check.csv",
+            str(output_base) + ".check.csv",
             "--cores=1",
         ]
     )
     assert result.returncode == 0
-    return (tacted, taxed, bbone)
 
 
 @pytest.mark.parametrize("execution_number", execution_number)
 @pytest.mark.parametrize("stem", ["weirdness", "intrusion", "short_branch", "stem"])
-def test_monophyly(script_runner, execution_number, datadir, stem):
-    tacted, taxed, bbone = run_tact(script_runner, datadir, stem)
+def test_monophyly(script_runner, execution_number, datadir, stem, tmp_path):
+    tacted, taxed, bbone = run_tact(script_runner, datadir, stem, tmp_path)
     extant = {x.taxon.label for x in bbone.leaf_nodes()}
     for node in taxed.postorder_internal_node_iter(exclude_seed_node=True):
         expected = {x.taxon.label for x in node.leaf_nodes()}
@@ -98,8 +99,8 @@ def test_monophyly(script_runner, execution_number, datadir, stem):
 
 @pytest.mark.parametrize("execution_number", execution_number)
 @pytest.mark.parametrize("stem", ["weirdness", "short_branch"])
-def test_short_branch(script_runner, execution_number, datadir, stem):
-    tacted, _taxed, _bbone = run_tact(script_runner, datadir, stem)
+def test_short_branch(script_runner, execution_number, datadir, stem, tmp_path):
+    tacted, _taxed, _bbone = run_tact(script_runner, datadir, stem, tmp_path)
     n_short = 0
     for leaf in tacted.leaf_node_iter():
         if leaf.edge.length < 0.1:
@@ -108,8 +109,8 @@ def test_short_branch(script_runner, execution_number, datadir, stem):
 
 
 @pytest.mark.parametrize("execution_number", execution_number)
-def test_stem_clade_attachment(script_runner, execution_number, datadir):
-    tacted, _taxed, _bbone = run_tact(script_runner, datadir, "stem2")
+def test_stem_clade_attachment(script_runner, execution_number, datadir, tmp_path):
+    tacted, _taxed, _bbone = run_tact(script_runner, datadir, "stem2", tmp_path)
     tacted.calc_node_ages()
     tacted.update_bipartitions()
     node = tacted.mrca(taxon_labels=["c1", "c2", "c3", "c4", "c5"])
